5702da7367c8a13a329bc71b2034467af4a7544e
Kelly Caylor <kcaylor@princeton.edu>
2013-11-12 22:16:56 +0200

    Removed pod deployment function

--- evepod.py
+++ evepod.py
@@ -7,14 +7,6 @@ def before_insert_data(documents):
 	print "A POST to data was just performed!"
 	for d in documents:
 		print "Posting " + d["s"] + " data from " + d["p"] + " to the database"
-		
-def deploy_new_pod(documents):
-	for d in documents:
-		print "DEPLOYING a new POD, " + d["urlid"] + " !"
-		
-def deploy_new_gateway(documents):
-	for d in documents:
-		print "DEPLOYING a new GATEWAY, " + d["urlid"] + " !"
 
 # Heroku defines a $PORT environment variable that we use to determine
 # if we're running locally or not.
@@ -31,7 +23,5 @@ if __name__ == '__main__':
 # Adding data to the system:
 	app.on_insert_data += before_insert_data
 # Administering pods, gateways, and sensors:
-	app.on_insert_pods += deploy_new_pod
-	app.on_insert_gateways += deploy_new_gateway
 # Run the program:
 	app.run(host=host, port=port, debug=True)
